07cd2de8caad4a6df4614bcf93f6701f19f1a8af
Siva Prabhaharan <sivaprabhaharan.j@gmail.com>
2024-10-15 13:38:24 +0530

    Create hello world lambda project

---
+++ task01/dnapp/lambdas/hello_world/Function.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using System.Reflection;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace SimpleLambdaFunction;
+
+public class Function
+{
+    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Body = "\"{\\\"statusCode\\\":200,\\\"message\\\":\\\"HellofromLambda\\\"}\"",
+            Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+        };
+    }
+}
