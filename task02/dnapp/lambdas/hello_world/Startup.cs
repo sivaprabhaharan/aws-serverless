0affc82ec1cf2a87edc65c942bfc6bbd19651fa2
Siva Prabhaharan <sivaprabhaharan.j@gmail.com>
2024-10-25 17:31:11 +0530

    Task 2  completed from local

---
+++ task02/dnapp/lambdas/hello_world/Startup.cs
@@ -0,0 +1,55 @@
+﻿using static Function.Controllers.ValuesController;
+
+namespace Function;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.Use(async (context, next) =>
+        {
+            if (context.Request.Path != "/hello")
+            {
+                var response = new LambdaResponse
+                {
+                    statusCode = 400,
+                    message = $"Bad request syntax or unsupported method. Request path: {context.Request.Path}. HTTP method: {context.Request.Method}"
+                };
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsJsonAsync(response);
+                return;
+            }
+            await next(context);
+        });
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
