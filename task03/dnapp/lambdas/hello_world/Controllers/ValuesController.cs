cbdab4953e3e86f466519e420a62cbfe88d44a0a
Siva Prabhaharan <sivaprabhaharan.j@gmail.com>
2024-11-08 00:18:44 +0530

    Task 3

---
+++ task03/dnapp/lambdas/hello_world/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Function.Controllers
+{
+    [Route("hello")]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public LambdaResponse Get()
+        {
+            return new LambdaResponse
+            {
+                statusCode = 200,
+                message = "Hello from Lambda"
+            };
+        }
+
+        public class LambdaResponse
+        {
+            public int statusCode { get; set; }
+            public required string message { get; set; }
+        }
+    }
+}
